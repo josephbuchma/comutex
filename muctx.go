082f57ace3bb61af1bfa8c7604b78327a1854f11
Yosyp Buchma <ybuchma@paloaltonetworks.com>
2021-08-24 20:25:59 +0300

    up

--- muctx.go
+++ muctx.go
@@ -1,10 +1,10 @@
 // This package allows to use functional composition with mutexes.
-// loctx
-// muctx
+// TODO: rename to loctx
 package muctx
 
 import (
 	"context"
+	"errors"
 	"sync"
 )
 
@@ -17,34 +17,76 @@ type RWLocker interface {
 	RUnlock()
 }
 
-func IsLocked(ctx context.Context, mu sync.Locker) bool {
-	return ctx.Value(mu) != nil
+// ErrLockUpgrade is returned on attempt to Lock()
+// RWMutex that is already locked with RLock.
+var ErrLockUpgrade = errors.New("mutex is already locked using RLock")
+
+type LockStatus uint32
+
+// Mutex lock statuses
+const (
+	Unlocked LockStatus = iota
+	RLocked
+	Locked
+)
+
+func (ls LockStatus) String() string {
+	switch ls {
+	case Unlocked:
+		return "unlocked"
+	case RLocked:
+		return "locked with RLock"
+	case Locked:
+		return "locked"
+	}
+	panic("invalid lock state")
+}
+
+// Status returns lock status in given context.
+func Status(ctx context.Context, mu sync.Locker) LockStatus {
+	v, _ := ctx.Value(mu).(LockStatus)
+	return v
 }
 
 // Lock actually locks mutex if given context is not "locked" already, and returns "locked" context.
 // If given context is already "locked", it returns it alongside with no-op unlock.
-func Lock(ctx context.Context, mu sync.Locker) (lockedCtx context.Context, unlock UnlockFn) {
-	if IsLocked(ctx, mu) {
-		return ctx, noop
+// If mutex is a RWMutex and is already locked with Rlock, it will return ErrLockUpgrade error.
+func Lock(ctx context.Context, mu sync.Locker) (context.Context, UnlockFn, error) {
+	if status := Status(ctx, mu); status > Unlocked {
+		if status != Locked {
+			return ctx, noop, ErrLockUpgrade
+		}
+		return ctx, noop, nil
 	}
 	mu.Lock()
-	return withMutex(ctx, mu, mu), wrapUnlock(mu.Unlock)
+	return withMutex(ctx, mu, Locked), wrapUnlock(mu.Unlock), nil
+}
+
+// MustLock actually locks mutex if given context is not "locked" already, and returns "locked" context.
+// If given context is already "locked", it returns it alongside with no-op unlock.
+// If mutex is a RWMutex and is already locked with Rlock, it will panic.
+func MustLock(ctx context.Context, mu sync.Locker) (lockedCtx context.Context, unlock UnlockFn) {
+	ctx, unlock, err := Lock(ctx, mu)
+	if err != nil {
+		panic(err)
+	}
+	return ctx, unlock
 }
 
 // RLock actually locks RW mutex for reading only if given context is not "locked" already, and returns "locked" context.
 // If given context is already "locked", it returns it alongside with no-op unlock.
 func RLock(ctx context.Context, mu RWLocker) (lockedCtx context.Context, unlock UnlockFn) {
-	if IsLocked(ctx, mu) {
+	if Status(ctx, mu) > Unlocked {
 		return ctx, noop
 	}
 	mu.RLock()
-	return withMutex(ctx, mu, mu), wrapUnlock(mu.RUnlock)
+	return withMutex(ctx, mu, RLocked), wrapUnlock(mu.RUnlock)
 }
 
-// Strip removes "unlocked" context without actually unlocking it.
-// It must be used when you pass context into new goroutine.
+// Strip returns "unlocked" context, but does not actually call Unlock on mutex.
+// Useful when you are passing locked context to a goroutine.
 func Strip(ctx context.Context, mu sync.Locker) context.Context {
-	return withMutex(ctx, mu, nil)
+	return withMutex(ctx, mu, Unlocked)
 }
 
 func wrapUnlock(unlock func()) func() bool {
@@ -54,8 +96,8 @@ func wrapUnlock(unlock func()) func() bool {
 	}
 }
 
-func withMutex(ctx context.Context, key sync.Locker, mu interface{}) context.Context {
-	return context.WithValue(ctx, key, mu)
+func withMutex(ctx context.Context, key sync.Locker, ls LockStatus) context.Context {
+	return context.WithValue(ctx, key, ls)
 }
 
 func noop() bool {
--- muctx_test.go
+++ muctx_test.go
@@ -9,6 +9,96 @@ import (
 
 var _ muctx.RWLocker = &mutMock{}
 
+func TestLock(t *testing.T) {
+	mu := &mutMock{}
+	ctx := context.Background()
+	if muctx.Status(ctx, mu) > muctx.Unlocked {
+		t.Errorf("Expected fresh mutext to be unlocked")
+	}
+	assertMutMockEql(t, mutMock{}, mu)
+	ctx, unlock, err := muctx.Lock(ctx, mu)
+	assertNilErr(t, err)
+	defer func() {
+		ok := unlock()
+		if !ok {
+			t.Errorf("Top level mutex unlock should return true")
+		}
+		assertMutMockEql(t, mutMock{1, 1, 0, 0}, mu)
+	}()
+	if muctx.Status(ctx, mu) == muctx.Unlocked {
+		t.Errorf("Expected mutex to be locked")
+	}
+	assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+
+	func(ctx context.Context) {
+		ctx2, unlock2, err := muctx.Lock(ctx, mu)
+		assertNilErr(t, err)
+		defer func() {
+			ok := unlock2()
+			if ok {
+				t.Errorf("Top level mutex unlock should return false")
+			}
+			assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+		}()
+		if muctx.Status(ctx2, mu) == muctx.Unlocked {
+			t.Errorf("Expected mutex to be locked")
+		}
+		assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+
+		func(ctx context.Context) {
+			ctx3, unlock3, err := muctx.Lock(ctx, mu)
+			assertNilErr(t, err)
+			defer func() {
+				ok := unlock3()
+				if ok {
+					t.Errorf("Top level mutex unlock should return false")
+				}
+				assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+			}()
+			if muctx.Status(ctx3, mu) == muctx.Unlocked {
+				t.Errorf("Expected mutex to be locked")
+			}
+			assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+		}(ctx2)
+	}(ctx)
+}
+
+func TestErrLockUpgrade(t *testing.T) {
+	ctx := context.Background()
+	mu := &mutMock{}
+	ctx, _ = muctx.RLock(ctx, mu)
+	ctx, _, err := muctx.Lock(ctx, mu)
+	if err != muctx.ErrLockUpgrade {
+		t.Errorf("expected ErrLockUpgrade")
+	}
+	if muctx.Status(ctx, mu) != muctx.RLocked {
+		t.Errorf("expected RLocked status")
+	}
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("Expected panic")
+			}
+		}()
+		muctx.MustLock(ctx, mu)
+	}()
+}
+
+func TestStrip(t *testing.T) {
+	ctx := context.Background()
+	mu := &mutMock{}
+
+	ctx, _ = muctx.MustLock(ctx, mu)
+	assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
+	sctx := muctx.Strip(ctx, mu)
+	if muctx.Status(sctx, mu) != muctx.Unlocked {
+		t.Error("status must be unlocked")
+	}
+	if muctx.Status(ctx, mu) != muctx.Locked {
+		t.Error("status must be locked")
+	}
+}
+
 type mutMock struct {
 	locked    int
 	unlocked  int
@@ -37,70 +127,8 @@ func assertMutMockEql(t *testing.T, expect mutMock, actual *mutMock) {
 	}
 }
 
-func TestLock(t *testing.T) {
-	func() {
-		mu := &mutMock{}
-		ctx := context.Background()
-		if muctx.IsLocked(ctx, mu) {
-			t.Errorf("Expected fresh mutext to be unlocked")
-		}
-		assertMutMockEql(t, mutMock{}, mu)
-		ctx, unlock := muctx.Lock(ctx, mu)
-		defer func() {
-			ok := unlock()
-			if !ok {
-				t.Errorf("Top level mutex unlock should return true")
-			}
-			assertMutMockEql(t, mutMock{1, 1, 0, 0}, mu)
-		}()
-		if !muctx.IsLocked(ctx, mu) {
-			t.Errorf("Expected mutex to be locked")
-		}
-		assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-
-		func(ctx context.Context) {
-			ctx2, unlock2 := muctx.Lock(ctx, mu)
-			defer func() {
-				ok := unlock2()
-				if ok {
-					t.Errorf("Top level mutex unlock should return false")
-				}
-				assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-			}()
-			if !muctx.IsLocked(ctx2, mu) {
-				t.Errorf("Expected mutex to be locked")
-			}
-			assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-
-			func(ctx context.Context) {
-				ctx3, unlock3 := muctx.Lock(ctx, mu)
-				defer func() {
-					ok := unlock3()
-					if ok {
-						t.Errorf("Top level mutex unlock should return false")
-					}
-					assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-				}()
-				if !muctx.IsLocked(ctx3, mu) {
-					t.Errorf("Expected mutex to be locked")
-				}
-				assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-			}(ctx2)
-		}(ctx)
-	}()
-}
-
-func TestStrip(t *testing.T) {
-	ctx := context.Background()
-	mu := &mutMock{}
-
-	ctx, _ = muctx.Lock(ctx, mu)
-	assertMutMockEql(t, mutMock{1, 0, 0, 0}, mu)
-	sctx := muctx.Strip(ctx, mu)
-	if muctx.IsLocked(sctx, mu) {
-		t.Error("IsLocked must return false")
-	}
-	if !muctx.IsLocked(ctx, mu) {
-		t.Error("IsLocked must return true")
+func assertNilErr(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("Expected nil error")
 	}
 }
