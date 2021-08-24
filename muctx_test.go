5d8cfbce9991993c9b55e838520958fe8a4ceb8d
Yosyp Buchma <ybuchma@paloaltonetworks.com>
2021-08-24 22:00:24 +0300

    full test coverage

--- muctx_test.go
+++ muctx_test.go
@@ -2,6 +2,7 @@ package comutex_test
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 
 var _ comutex.RWLocker = &mutMock{}
 
-func TestLock(t *testing.T) {
+func TestRLock(t *testing.T) {
 	mu := &mutMock{}
 	ctx := context.Background()
 	originalCtx := ctx
@@ -18,8 +19,7 @@ func TestLock(t *testing.T) {
 		t.Errorf("Expected fresh mutext to be unlocked")
 	}
 	assertMutMockEql(t, mutState{}, mu)
-	ctx, unlock, err := comutex.Lock(ctx, mu)
-	assertNilErr(t, err)
+	ctx, unlock := comutex.RLock(ctx, mu)
 	defer func() {
 		unlockedCtx := unlock()
 		if isLocked(unlockedCtx, mu) {
@@ -28,11 +28,66 @@ func TestLock(t *testing.T) {
 		if originalCtx != unlockedCtx {
 			t.Errorf("unlock should return original context")
 		}
-		assertMutMockEql(t, mutState{1, 1, 0, 0}, mu)
+		assertMutMockEql(t, mutState{0, 0, 1, 1}, mu)
 	}()
 	if comutex.Status(ctx, mu) == comutex.Unlocked {
 		t.Errorf("Expected mutex to be locked")
 	}
+	assertMutMockEql(t, mutState{0, 0, 1, 0}, mu)
+
+	func(ctx context.Context) {
+		ctx2, unlock2 := comutex.RLock(ctx, mu)
+		defer func() {
+			if !isLocked(unlock2(), mu) {
+				t.Errorf("Nested mutex must remain locked after unlock")
+			}
+			assertMutMockEql(t, mutState{0, 0, 1, 0}, mu)
+		}()
+		if comutex.Status(ctx2, mu) == comutex.Unlocked {
+			t.Errorf("Expected mutex to be locked")
+		}
+		assertMutMockEql(t, mutState{0, 0, 1, 0}, mu)
+
+		func(ctx context.Context) {
+			ctx3, unlock3 := comutex.RLock(ctx, mu)
+			defer func() {
+				if !isLocked(unlock3(), mu) {
+					t.Errorf("Nested mutex must remain locked after unlock")
+				}
+				assertMutMockEql(t, mutState{0, 0, 1, 0}, mu)
+			}()
+			if comutex.Status(ctx3, mu) == comutex.Unlocked {
+				t.Errorf("Expected mutex to be locked")
+			}
+			assertMutMockEql(t, mutState{0, 0, 1, 0}, mu)
+		}(ctx2)
+	}(ctx)
+}
+
+func assertStatus(t *testing.T, ctx context.Context, mu sync.Locker, status comutex.LockStatus) {
+	s := comutex.Status(ctx, mu)
+	if s != status {
+		t.Errorf("expected lock status %q, got %q", status, status)
+	}
+}
+
+func TestLock(t *testing.T) {
+	mu := &mutMock{}
+	ctx := context.Background()
+	originalCtx := ctx
+	assertStatus(t, ctx, mu, comutex.Unlocked)
+	assertMutMockEql(t, mutState{}, mu)
+	ctx, unlock, err := comutex.Lock(ctx, mu)
+	assertNilErr(t, err)
+	defer func() {
+		unlockedCtx := unlock()
+		assertStatus(t, unlockedCtx, mu, comutex.Unlocked)
+		if originalCtx != unlockedCtx {
+			t.Errorf("unlock should return original context")
+		}
+		assertMutMockEql(t, mutState{1, 1, 0, 0}, mu)
+	}()
+	assertStatus(t, ctx, mu, comutex.Locked)
 	assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
 
 	func(ctx context.Context) {
@@ -44,9 +99,7 @@ func TestLock(t *testing.T) {
 			}
 			assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
 		}()
-		if comutex.Status(ctx2, mu) == comutex.Unlocked {
-			t.Errorf("Expected mutex to be locked")
-		}
+		assertStatus(t, ctx2, mu, comutex.Locked)
 		assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
 
 		func(ctx context.Context) {
@@ -58,9 +111,7 @@ func TestLock(t *testing.T) {
 				}
 				assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
 			}()
-			if comutex.Status(ctx3, mu) == comutex.Unlocked {
-				t.Errorf("Expected mutex to be locked")
-			}
+			assertStatus(t, ctx3, mu, comutex.Locked)
 			assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
 		}(ctx2)
 	}(ctx)
@@ -101,6 +152,20 @@ func TestWithStatusUnlocked(t *testing.T) {
 		t.Error("status must be unlocked")
 	}
 	assertMutMockEql(t, mutState{1, 0, 0, 0}, mu)
+}
+
+func TestLockStatus(t *testing.T) {
+	for _, status := range []comutex.LockStatus{0, 1, 2} {
+		if fmt.Sprintf("%d", status) == status.String() {
+			t.Errorf("missing string for status %d", status)
+		}
+	}
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("calling String on invalid lock status did not panic")
+		}
+	}()
+	_ = comutex.LockStatus(3).String()
 }
 
 func isLocked(ctx context.Context, mu sync.Locker) bool {
